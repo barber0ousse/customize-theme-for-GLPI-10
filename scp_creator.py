d3ddb7860f978c7063727952270e51a7d8f261ba
BarbeR0ousse <48182301+barber0ousse@users.noreply.github.com>
2022-05-12 09:58:32 +0200

    Update scp_creator.py

--- scp_creator.py
+++ scp_creator.py
@@ -3,8 +3,8 @@
 import os
 
 windows_path = r"" # Path to your custom files "C:\Users\%userprofile%\glpi"
-separator = "\\"
 destination = r"" # Your ssh login to your GLPI server "demo@ip"
+separator = "\\"
 
 with open("scp.ps1", "w") as script:
     for path, dirs, files in os.walk(windows_path):
