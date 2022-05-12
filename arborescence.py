b36f49f01998e04fe1097c2943350a887424f15c
BarbeR0ousse <48182301+barber0ousse@users.noreply.github.com>
2022-05-12 09:34:23 +0200

    Update arborescence.py

--- arborescence.py
+++ arborescence.py
@@ -2,13 +2,13 @@
 
 import os
 
-windows_path = r"C:\Users\dimitri.clary\GLPI\glpi"
-Separator = "\\"
-destination = r"admin-si@tipi.tisseo-ingenierie.fr"
+windows_path = r"" # Path to your custom files "C:\Users\%userprofile%\glpi"
+separator = "\\"
+destination = r"" # Your ssh login to your GLPI server "demo@ip"
 
 with open("scp.ps1", "w") as script:
     for path, dirs, files in os.walk(windows_path):
         for filename in files:
-            linux_path = path[len(windows_path):].replace("\\", "/")
-            command = "scp " + path + "\\" + filename +" " + destination + ":/var/www/html/glpi" + linux_path + "\r"
+            linux_path = ":/var/www/html/glpi" + path[len(windows_path):].replace(separator, "/")
+            command = "scp " + path + separator + filename + " " + destination + linux_path + "\r"
             script.write(command)
